d932150d3225e559d83f6ca26f49433346bd56ef
Philip Eskins and Ryan McLean <ryan@>
2019-03-11 16:07:07 -0700

    add grid for cards to sit in. add function to flip cards and function to only allow two cards to be open at one point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import $ from 'jquery';
 import ('./scss/input.scss');
 
-$(".card").on("click", $(".front"), function() {
-  $(".back").toggle();
-});
+var openedCard = [];
+
+$(".card").click(function() {
+  var checkNum = checkCards();
+  if (checkNum >= 2) {
+    $(".back").hide();
+    $(".front").show();
+    $(".card").removeClass("active");
+  }
+  else {
+    $(this).find(".back").toggle();
+    $(this).find(".front").toggle();
+    $(this).addClass("active");
+    checkMatch();
+  }
+})
+
+function checkCards() {
+  var numActive = $(".active").length;
+  return numActive;
+}
+
+function checkMatch() {
+  var groot = 0;
+  var children = $(".gameContainer").children();
+  children.each(function() {
+    if ($(".card").hasClass("alien")) {
+      groot++;
+      console.log(groot);
+    }
+  });
+  var checkClass = $(this).className;
+  console.log(checkClass);
+}
